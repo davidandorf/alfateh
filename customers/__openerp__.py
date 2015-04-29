3e5d8e1f025820d95a5a67c1a8bc0723b7375d48
david@brokergenius.com <david@brokergenius.com>
2015-04-29 18:53:41 +0500

    customers

---
+++ customers/__openerp__.py
@@ -0,0 +1,10 @@
+{
+'name': "Alfateh Customers",
+'version': "1.0",
+'author': "Hammad",
+'category': "apps",
+'depends': ['mail','sales'],
+'data': ['customers.xml'],
+'demo': [],
+'installable': True,
+}
