832a81cefd37d7c8211232144a4c466e808e7a20
david@brokergenius.com <david@brokergenius.com>
2015-05-07 08:41:51 +0500

    warehouse

---
+++ products/__openerp__.py
@@ -0,0 +1,10 @@
+{
+'name': "Alfateh Products",
+'version': "1.0",
+'author': "Hammad",
+'category': "apps",
+'depends': ['mail'],
+'data': ['products.xml'],
+'demo': [],
+'installable': True,
+}
---
+++ warehouse/__openerp__.py
@@ -0,0 +1,10 @@
+{
+'name': "Alfateh Warehouse",
+'version': "1.0",
+'author': "Hammad",
+'category': "apps",
+'depends': ['mail'],
+'data': ['warehouse.xml'],
+'demo': [],
+'installable': True,
+}
