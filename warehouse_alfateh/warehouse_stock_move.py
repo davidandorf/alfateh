b1320edc3afc72451f6ced325b672f606431dd28
david@brokergenius.com <david@brokergenius.com>
2015-05-19 21:40:01 +0500

    product chnges

--- warehouse_alfateh/warehouse_stock_move.py
+++ warehouse_alfateh/warehouse_stock_move.py
@@ -37,3 +37,45 @@ class warehouse_stock_move_custom(models.Model):
         if loc_dest_id:
             result['location_dest_id'] = loc_dest_id
         return {'value': result}
+    
+    def _get_invoice_line_vals(self, cr, uid, move, partner, inv_type, context=None):
+        fp_obj = self.pool.get('account.fiscal.position')
+        # Get account_id
+        product = self.pool.get('product.product').browse(cr, uid, move.product_id.id)[0]
+        
+        taxes = False
+        if inv_type in ('out_invoice', 'out_refund'):
+            account_id = move.product_id.property_account_income.id
+            if not account_id:
+                account_id = move.product_id.categ_id.property_account_income_categ.id
+            taxes = product.taxes_id
+        else:
+            account_id = move.product_id.property_account_expense.id
+            taxes = product.supplier_taxes_id
+            if not account_id:
+                account_id = move.product_id.categ_id.property_account_expense_categ.id
+        fiscal_position = partner.property_account_position
+        account_id = fp_obj.map_account(cr, uid, fiscal_position, account_id)
+            
+        # set UoS if it's a sale and the picking doesn't have one
+        uos_id = move.product_uom.id
+        quantity = move.product_uom_qty
+        if move.product_uos:
+            uos_id = move.product_uos.id
+            quantity = move.product_uos_qty
+        result = {
+            'name': move.name,
+            'account_id': account_id,
+            'product_id': move.product_id.id,
+            'uos_id': uos_id,
+            'quantity': quantity,
+            'price_unit': self._get_price_unit_invoice(cr, uid, move, inv_type),
+            'discount': 0.0,
+            'account_analytic_id': False,
+            'sale_type':move.sale_type,
+            'schedule_no':move.schedule_no,
+            'item_sr_no':move.item_sr_no,
+            'invoice_line_tax_id':taxes
+        }
+        print(result)
+        return result
