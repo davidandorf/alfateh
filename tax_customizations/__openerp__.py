4e6cfb1fa36f9cee65b4851b592c6b5b463e5b50
david@brokergenius.com <david@brokergenius.com>
2015-07-07 17:54:46 +0500

    taxes customization

---
+++ tax_customizations/__openerp__.py
@@ -0,0 +1,10 @@
+{
+'name': "tax_customization",
+'version': "1.0",
+'author': "Hammad",
+'category': "apps",
+'depends': ['account'],
+'data': ['tax_customization.xml'],
+'demo': [],
+'installable': True,
+}
